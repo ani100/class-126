710037220bdb5a9c621dc44e56e83ddf348bbc64
ani100 <85432450+ani100@users.noreply.github.com>
2023-05-17 15:31:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+function setup() {
+
+canvas = createCanvas(100,100);
+canvas.position(100,100);
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+function draw() {
+
+    background("black");
+
+}
